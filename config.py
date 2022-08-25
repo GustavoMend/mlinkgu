7f9e26756b358ba59ff6f94fa65fe8f7934e3328
GustavoMend <73637609+GustavoMend@users.noreply.github.com>
2022-08-24 23:29:08 -0500

    Update config.py

--- config.py
+++ config.py
@@ -19,13 +19,13 @@ class Config(object):
     Bot_username = os.environ.get("Bot_username", "@MlinkG_bot") # Your bot's telegram username (must enter with '@' in the front of the username)
     #If deploying on vps edit the above value as example := Bot_username = "Your-Bot_username-inside-inverted-commas."
     
-    OWNER_ID = os.environ.get("OWNER_ID", None) # Your(owner's) telegram id
+    OWNER_ID = os.environ.get("OWNER_ID", "1276772579") # Your(owner's) telegram id
     #If deploying on vps edit the above value as example := OWNER_ID = Your-telegram id-without-inverted-commas
     
-    REDIS_URI = os.environ.get("REDIS_URI", None) # Get This Value from http://redislabs.com/try-free (If you don't know how to obtain the a video tutorial is available here:- https://t.me/botzupdate/5)
+    REDIS_URI = os.environ.get("REDIS_URI", "redis-11834.c84.us-east-1-2.ec2.cloud.redislabs.com:11834") # Get This Value from http://redislabs.com/try-free (If you don't know how to obtain the a video tutorial is available here:- https://t.me/botzupdate/5)
     #If deploying on vps edit the above value as example := REDIS_URI = "Your-Redis-Endpoint-inside-inverted-commas."
     
-    REDIS_PASS = os.environ.get("REDIS_PASS", None) # Get This Value from http://redislabs.com/try-free (If you don't know how to obtain the a video tutorial is available here:- https://t.me/botzupdate/5)
+    REDIS_PASS = os.environ.get("REDIS_PASS", "rrKF5VZ7HyOjAiXaTHNCDoaSsL0sv7ef") # Get This Value from http://redislabs.com/try-free (If you don't know how to obtain the a video tutorial is available here:- https://t.me/botzupdate/5)
     #If deploying on vps edit the above value as example := REDIS_PASS = "Your-Redis-Password-inside-inverted-commas."
 
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split()) # Id's of the telegram users, who you want to allow for multitasking - downloading multiple links at once!
